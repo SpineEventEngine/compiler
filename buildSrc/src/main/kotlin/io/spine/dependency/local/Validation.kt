49d88b4c7cf26a58c76fafd0872d2a88364f405c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-12-10 20:07:51 +0000

    Update build scripts from CoreJvm

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
@@ -36,7 +36,7 @@ object Validation {
     /**
      * The version of the Validation library artifacts.
      */
-    const val version = "2.0.0-SNAPSHOT.372"
+    const val version = "2.0.0-SNAPSHOT.373"
 
     /**
      * The last version of Validation compatible with ProtoData.
