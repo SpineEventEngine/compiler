86d24cc5259999b0e5fd59f1f3920b740379b498
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-06-25 16:06:59 +0100

    Fix using "which" in docs

--- api/src/test/java/io/spine/tools/compiler/settings/given/JavaActionNoParam.java
+++ api/src/test/java/io/spine/tools/compiler/settings/given/JavaActionNoParam.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025, TeamDev. All rights reserved.
+ * Copyright 2026, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ import io.spine.tools.code.Java;
 import org.jspecify.annotations.NonNull;
 
 /**
- * A stub rendering action implemented in Java which accepts no parameter.
+ * A stub rendering action implemented in Java that accepts no parameter.
  */
 public class JavaActionNoParam extends ExposeParam<Empty> {
 
